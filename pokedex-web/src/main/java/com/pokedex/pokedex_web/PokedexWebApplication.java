3e5b56e2f2758e353b2b99a4ae9a85431bd31088
Ken <ahmadsoud63@gmail.com>
2025-04-12 16:54:51 +0300

    Intilize spring boot project

---
+++ pokedex-web/src/main/java/com/pokedex/pokedex_web/PokedexWebApplication.java
@@ -0,0 +1,13 @@
+package com.pokedex.pokedex_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedexWebApplication.class, args);
+	}
+
+}
---
+++ pokedex-web/src/test/java/com/pokedex/pokedex_web/PokedexWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokedex.pokedex_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
