d98e3df28bdbd584bf01834898c67101ef2635aa
Ken <ahmadsoud63@gmail.com>
2025-04-12 18:34:04 +0300

    add Home controller

---
+++ pokedex-web/src/main/java/Controllers/HomeController.java
@@ -0,0 +1,13 @@
+package Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "index"; // will look for index.html or index.jsp in templates
+    }
+}
