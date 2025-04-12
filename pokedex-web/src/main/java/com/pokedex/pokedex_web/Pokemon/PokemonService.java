7c1fca25fff9ea0a94153c2b6a1421ebc9e80add
Ken <ahmadsoud63@gmail.com>
2025-04-12 23:08:37 +0300

    modify pokemon service

--- pokedex-web/src/main/java/com/pokedex/pokedex_web/Pokemon/PokemonService.java
+++ pokedex-web/src/main/java/com/pokedex/pokedex_web/Pokemon/PokemonService.java
@@ -15,26 +15,22 @@ public class PokemonService {
         String url = "https://pokeapi.co/api/v2/pokemon/" + name.toLowerCase();
         Map<String, Object> response = restTemplate.getForObject(url, Map.class);
 
-        Pokemon pokemon = new Pokemon();
-        pokemon.setName((String) response.get("name"));
-        pokemon.setHeight((Integer) response.get("height"));
-
+        int height=(Integer) response.get("height");
+        
         List<String> abilities = ((List<Map<String, Object>>) response.get("abilities"))
-                .stream()
-                .map(a -> (String) ((Map<String, Object>) a.get("ability")).get("name"))
-                .toList();
-
+        .stream()
+        .map(a -> (String) ((Map<String, Object>) a.get("ability")).get("name"))
+        .toList();
+        
         List<String> moves = ((List<Map<String, Object>>) response.get("moves"))
-                .stream()
-                .limit(5)
-                .map(m -> (String) ((Map<String, Object>) m.get("move")).get("name"))
-                .toList();
-
+        .stream()
+        .limit(5)
+        .map(m -> (String) ((Map<String, Object>) m.get("move")).get("name"))
+        .toList();
+        
         String sprite = (String) ((Map<String, Object>) response.get("sprites")).get("front_default");
-
-        pokemon.setAbilities(abilities);
-        pokemon.setMoves(moves);
-        pokemon.setSprite(sprite);
+        
+        Pokemon pokemon = new Pokemon(name,height,abilities,moves,sprite);
 
         return pokemon;
     }
